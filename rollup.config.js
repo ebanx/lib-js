1101b63165772450d1d470ac2b5397379efb1bf2
Bruno Couto <brunodev02221@gmail.com>
2019-03-28 08:31:09 -0300

    Update building process
    - Replace uglify by terser
    - Set browserlist for babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import json from 'rollup-plugin-json';
-import babel from 'rollup-plugin-babel';
-import { uglify } from 'rollup-plugin-uglify';
+import json from "rollup-plugin-json";
+import babel from "rollup-plugin-babel";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/ebanx.js",
@@ -13,8 +13,15 @@ export default {
     json(),
     babel({
       babelrc: false,
-      presets: ["@babel/env"]
+      presets: [
+        [
+          "@babel/env",
+          {
+            targets: "> 0.5%, ie >= 10"
+          }
+        ]
+      ]
     }),
-    uglify()
+    terser()
   ]
-}
+};
