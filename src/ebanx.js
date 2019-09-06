038e81538d0d0580bcb72eed3e8d0f441cfa493f
Ruanito Santos <ruanito.santos@ebanx.com>
2019-09-06 10:18:22 -0300

    Add Bolivia support

--- src/ebanx.js
+++ src/ebanx.js
@@ -314,7 +314,7 @@ EBANX.utils = (function () {
         return (EBANX.config.isLive() ? 'https://api.ebanxpay.com/' : 'https://sandbox.ebanxpay.com/');
       }
     },
-    availableCountries: ['br', 'mx', 'co', 'ar', 'pe', 'cl', 'ec'].join(', '),
+    availableCountries: ['br', 'mx', 'co', 'ar', 'pe', 'cl', 'ec', 'bo'].join(', '),
     creditCardScheme: function (cardNumber) {
       EBANX.validator.card.validateNumber(cardNumber);
 
