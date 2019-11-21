8349f0aa2ed0004702abc02e37479b191ed1d705
henriquemenegao <henriique.sjrp@gmail.com>
2019-11-21 14:41:30 -0300

    Created base rollup-config and rollup-local / rollup-xborder

---
+++ rollup-local.config.js
@@ -0,0 +1,18 @@
+import replace from '@rollup/plugin-replace';
+import config from './rollup-base.config';
+import project from './package.json';
+
+export default {
+  ...config,
+  output: {
+    file: `dist/ebanxpay-${project.version}.min.js`,
+    ...config.output
+  },
+  plugins: [
+    replace({
+      'process.env.EBANX_API_PRODUCTION': JSON.stringify('https://api.ebanx.com.br/'),
+      'process.env.EBANX_API_SANDBOX': JSON.stringify('https://staging.ebanx.com.br/')
+    }),
+    ...config.plugins
+  ]
+};
