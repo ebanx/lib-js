39b82c6b7d6fe0736913223958e1a8e970600606
lmarqs <lucas.marques@ebanx.com>
2020-03-10 17:31:36 -0300

    refact tokenize.ts

--- src/$public/tokenize.ts
+++ src/$public/tokenize.ts
@@ -1,18 +1,25 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { http } from './http';
+import { config } from './config';
+import { utils } from './utils';
+
+
 export const tokenize = {
   card: {
     token: function (cardData, cb, errorCallback) {
       const tokenResource = EBANX.utils.api.resources.createToken();
 
-      EBANX.http.ajax.request({
-        url: tokenResource.url,
-        method: tokenResource.method,
-        data: JSON.stringify({
-          public_integration_key: EBANX.config.getPublishableKey(),
-          payment_type_code: EBANX.utils.creditCardScheme(cardData.card_number),
-          country: EBANX.config.getCountry(),
-          card: cardData,
-        }),
-      })
+      http.ajax
+        .request({
+          url: tokenResource.url,
+          method: tokenResource.method,
+          data: JSON.stringify({
+            public_integration_key: EBANX.config.getPublishableKey(),
+            payment_type_code: utils.creditCardScheme(cardData.card_number),
+            country: config.getCountry(),
+            card: cardData,
+          }),
+        })
         .always(function (result) {
           if (result.status === 'ERROR' || !('token' in result)) {
             return errorCallback(result);
