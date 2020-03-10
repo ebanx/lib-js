613a19e77a4f11dab0f7e7fc1d9606e8f0b8db8e
lmarqs <lucas.marques@ebanx.com>
2020-03-10 17:28:20 -0300

    finish code splitting

--- src/$public/$public.ts
+++ src/$public/$public.ts
@@ -1,13 +1,19 @@
+import { card } from './card';
 import { config } from './config';
+import { deviceFingerprint } from './fingerprint';
 import { errors } from './errors';
 import { http } from './http';
+import { tokenize } from './tokenize';
 import { utils } from './utils';
 import { validator } from './validator';
 
 export const $public = {
+  card,
   config,
+  deviceFingerprint,
   errors,
   http,
+  tokenize,
   utils,
   validator,
 };
