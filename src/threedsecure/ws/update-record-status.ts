47dce5f2097f9d82f80f394d0fb21bc561cfd86c
kevintesta <66317451+kevintesta@users.noreply.github.com>
2022-01-04 16:54:58 -0300

    [t-HNIg9pzA] 3DS - update record status when cardinal error is returned

--- src/threedsecure/ws/update-record-status.ts
+++ src/threedsecure/ws/update-record-status.ts
@@ -1,10 +1,12 @@
-export async function updateRecordStatus(actionCode: string, merchantTrackId: string,): Promise<void> {
+export async function updateRecordStatus(actionCode: string, merchantTrackId: string, errorNumber?: string, errorMessage?: string): Promise<void> {
   const url = new URL('/ws/threedsecureserver-record', EBANX.utils.api.path());
 
   const data = {
     status: actionCode,
     merchantTrackId,
     publicIntegrationKey: EBANX.config.getPublishableKey(),
+    errorNumber,
+    errorMessage,
   };
   
   await fetch(url.toString(), {
