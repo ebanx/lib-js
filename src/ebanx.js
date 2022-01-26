6566ff22ca6d7f2f35d1bc2985907a7de5583c9d
Rafael Garcia <rafael.carvalho@ebanx.com>
2022-01-26 10:23:12 -0300

    [t-25blSFMd] Notify Pay via Http call when Fingerprint errors occurs on lib-js - handling missing publicIntegrationKey

--- src/ebanx.js
+++ src/ebanx.js
@@ -420,6 +420,12 @@ EBANX.utils = (function () {
         url: utilsModule.api.path() + 'fingerprint/provider',
         method: 'get'
       };
+    },
+    fingerprintErrorResource: function () {
+      return {
+        url: utilsModule.api.path() + 'fingerprint/error',
+        method: 'get'
+      };
     }
   };
 
@@ -660,22 +666,50 @@ EBANX.deviceFingerprint = {
           this.loadProvider(provider, this.saveProviderSessionList.bind(this));
         }.bind(this));
       } catch (e) {
+        this.notifyPayOnError(e);
         this.onErrorCallback(e);
       }
     }.bind(this));
   },
 
   getList: function (cb) {
-    EBANX.http.ajax
-      .request({
-        url: EBANX.utils.api.resources.fingerPrintResource().url,
-        data: {
-          publicIntegrationKey: EBANX.config.getPublishableKey(),
-          country: EBANX.config.getCountry(),
-          origin: window.location.pathname,
-        }
-      })
-      .always(cb);
+    try {
+      EBANX.http.ajax
+        .request({
+          url: EBANX.utils.api.resources.fingerPrintResource().url,
+          data: {
+            publicIntegrationKey: EBANX.config.getPublishableKey(),
+            country: EBANX.config.getCountry(),
+            origin: window.location.pathname,
+          }
+        })
+        .always(cb);
+    } catch (e) {
+      this.notifyPayOnError(e);
+    }
+  },
+
+  notifyPayOnError: function (error) {
+    let pik;
+
+    try {
+      pik = EBANX.config.getPublishableKey();
+    } catch (e) {
+      pik = '';
+    }
+
+    var data = {
+      publicIntegrationKey: pik,
+      errorMessage: error.message
+    };
+
+    var resource = EBANX.utils.api.resources.fingerprintErrorResource();
+
+    EBANX.http.ajax.request({
+      url: resource.url,
+      method: resource.method,
+      data: data
+    })
   },
 
   saveProviderSessionList: function (providerSession) {
